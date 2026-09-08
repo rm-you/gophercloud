34a6113c44ebb1821448d6a42873bb588ea474ef
Adam Harwell <flux.adam@gmail.com>
2026-09-08 17:25:17 -0400

    feat(identity): add OIDC client credentials (#3942)

--- openstack/client.go
+++ openstack/client.go
@@ -11,6 +11,7 @@ import (
 	tokens2 "github.com/gophercloud/gophercloud/v2/openstack/identity/v2/tokens"
 	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/ec2tokens"
 	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/oauth1"
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/oidc"
 	tokens3 "github.com/gophercloud/gophercloud/v2/openstack/identity/v3/tokens"
 	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/websso"
 	"github.com/gophercloud/gophercloud/v2/openstack/utils"
@@ -230,6 +231,8 @@ func v3auth(ctx context.Context, client *gophercloud.ProviderClient, endpoint st
 			result = oauth1.Create(ctx, v3Client, opts)
 		case *websso.AuthOptions:
 			result = websso.Authenticate(ctx, v3Client, opts)
+		case *oidc.AuthOptions:
+			result = oidc.Create(ctx, v3Client, opts)
 		default:
 			result = tokens3.Create(ctx, v3Client, opts)
 		}
@@ -275,6 +278,10 @@ func v3auth(ctx context.Context, client *gophercloud.ProviderClient, endpoint st
 			o.AllowReauth = false
 			tao = &o
 		case *websso.AuthOptions:
+			o := *ot
+			o.AllowReauth = false
+			tao = &o
+		case *oidc.AuthOptions:
 			o := *ot
 			o.AllowReauth = false
 			tao = &o
